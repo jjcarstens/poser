50f028418e40bcce94cc77dee5863def600d6f73
Jon Carstens <jjcarstens@me.com>
2021-02-01 11:27:22 -0700

    Fix errors in dev.exs

--- config/dev.exs
+++ config/dev.exs
@@ -1,18 +1,17 @@
 import Config
 
-# Shared Configuration.
-config :nerves_hub_link,
-  ca_certs: Path.expand("../ssl/dev", __DIR__)
+ca_certs = Path.expand("../ssl/dev", __DIR__)
 
 # API HTTP connection.
 config :nerves_hub_user_api,
+  ca_certs: ca_certs,
   host: "0.0.0.0",
   port: 4002,
-  server_name_indication: 'api.nerves-hub.org',
-  ca_certs: Path.expand("../ssl/dev", __DIR__)
+  server_name_indication: 'api.nerves-hub.org'
 
 # Device HTTP connection.
 config :nerves_hub_link,
+  ca_certs: ca_certs,
   device_api_host: "0.0.0.0",
   device_api_port: 4001,
-  server_name_indication: 'api.nerves-hub.org'
+  ssl: [server_name_indication: 'device.nerves-hub.org']
