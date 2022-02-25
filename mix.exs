fd4a6c01eb02c25dec5088593e209a0ef0688c83
Jon Carstens <jjcarstens@me.com>
2022-02-25 15:01:50 -0700

    Cleanup unused deps

--- mix.exs
+++ mix.exs
@@ -37,9 +37,7 @@ defmodule Poser.MixProject do
       {:nerves_runtime, "~> 0.11.3", override: true},
 
       # Dependencies for all targets except :host
-      {:nerves_pack, "~> 0.4", targets: @all_targets},
-      {:nerves_system_rpi0, "~> 1.13", runtime: false, targets: :rpi0},
-      {:nerves_system_x86_64, "~> 1.13", runtime: false, targets: :x86_64}
+      {:nerves_pack, "~> 0.4", targets: @all_targets}
     ]
   end
 
